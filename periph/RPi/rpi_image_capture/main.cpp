4dab093cc4815d4c522561817699a94c838cb1df
unnamedStargazer <felix.ess@gmail.com>
2023-12-05 22:28:10 -0800

    LCD working with Raspberry Pi

--- periph/RPi/rpi_image_capture/main.cpp
+++ periph/RPi/rpi_image_capture/main.cpp
@@ -2,18 +2,41 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-using namespace cv;
+#include <thread>
+#include "header.h"
+
+//using namespace cv;
+
+// Test functions
+extern int lcd_test();
+
+// LCD functions
+extern void lcd_init();
+extern void typeInt(int i);
+extern void typeFloat(float myFloat);
+extern void typeString();
 
 int main(int argc, char *argv[])
 {
-    Mat img = imread("arnold_schwarzenegger.jpg");
+    std::thread displayThread(lcd_test);
+    displayThread.detach();
+
+    std::thread spiThread(lcd_test);
+    spiThread.detach();
+
+    cv::Mat img = cv::imread("arnold_schwarzenegger.jpg");
+
     if(img.empty())
        return -1;
-    namedWindow("arnold_schwarzenegger", cv::WINDOW_AUTOSIZE );
-    imshow("arnold_schwarzenegger", img);
-    waitKey(0);
+    cv::namedWindow("arnold_schwarzenegger", cv::WINDOW_AUTOSIZE );
+    cv::imshow("arnold_schwarzenegger", img);
+
+    cv::waitKey(0);
+
     return 0;
 }
+
+
 
 
 
